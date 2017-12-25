8f4ff8ec14d0d6c22d0b0443edc767c69b4b013a
hester <garuhester@gmail.com>
2017-12-25 23:24:53 +0800

    1.发布文章 2.文章添加分类并在个人中心显示

--- routes/index.js
+++ routes/index.js
@@ -1,117 +1,124 @@
-var login = require('./login');
-var register = require('./register');
-var getIndexData = require('./getIndexData');
-var setting = require('./setting');
-var write = require('./write');
-var personal = require('./personal');
-var follow = require('./follow');
-var article = require('./article');
+var login = require("./login");
+var register = require("./register");
+var getIndexData = require("./getIndexData");
+var setting = require("./setting");
+var write = require("./write");
+var personal = require("./personal");
+var follow = require("./follow");
+var article = require("./article");
 
 //日期插件
-var moment = require('moment');
+var moment = require("moment");
 
-var multer = require('multer');
+var multer = require("multer");
 var storage = multer.diskStorage({
-    destination: 'static/headimage',
-    filename: function(req, file, cb) {
+    destination: "static/headimage",
+    filename: function (req, file, cb) {
         var id = req.session.user.id;
-        var end = file.originalname.split('.')[1];
+        var end = file.originalname.split(".")[1];
         cb(null, `${id}.${end}`);
     }
 });
-var upload = multer({ storage });
+var upload = multer({
+    storage
+});
 
-module.exports = function(app) {
-
+module.exports = function (app) {
     //ejs中使用的函数
-    app.locals.dateFormat = function(date) {
-        return moment(date).format('YYYY-MM-DD HH:mm:ss');
-    }
+    app.locals.dateFormat = function (date) {
+        return moment(date).format("YYYY-MM-DD HH:mm:ss");
+    };
 
-    app.use(upload.array('headimage'));
+    app.use(upload.array("headimage"));
 
     //登录
-    app.get('/login', function(req, res) {
-        res.render('login', {
-            title: '登录博客'
+    app.get("/login", function (req, res) {
+        res.render("login", {
+            title: "登录博客"
         });
     });
     //登录请求
-    app.post('/login', login.doLogin);
+    app.post("/login", login.doLogin);
 
     //注册
-    app.get('/register', function(req, res) {
-        res.render('register', {
-            title: '注册博客'
+    app.get("/register", function (req, res) {
+        res.render("register", {
+            title: "注册博客"
         });
     });
     //注册请求
-    app.post('/register', register.doRegister);
+    app.post("/register", register.doRegister);
 
-    //首页 
-    app.get('/', function(req, res) {
+    //首页
+    app.get("/", function (req, res) {
         var currentPage = req.query.page || 1;
-        var articleName = req.query.articlename || 'nosearch';
+        var articleName = req.query.articlename || "nosearch";
         if (req.session.user) {
-            getIndexData.getData(req.session.user.id, currentPage, articleName).then(function(data) {
-                res.render('index', {
-                    title: '渐变-首页',
-                    user: req.session.user,
-                    data,
+            getIndexData
+                .getData(req.session.user.id, currentPage, articleName)
+                .then(function (data) {
+                    res.render("index", {
+                        title: "渐变-首页",
+                        user: req.session.user,
+                        data
+                    });
                 });
-            });
         } else {
-            getIndexData.getData(null, currentPage, articleName).then(function(data) {
-                res.render('index', {
-                    title: '渐变-首页',
-                    user: 'no',
-                    data,
+            getIndexData
+                .getData(null, currentPage, articleName)
+                .then(function (data) {
+                    res.render("index", {
+                        title: "渐变-首页",
+                        user: "no",
+                        data
+                    });
                 });
-            });
         }
     });
 
     //关注
-    app.get('/follow/:id', function(req, res) {
+    app.get("/follow/:id", function (req, res) {
         if (req.session.user) {
             var selfid = req.session.user.id;
             var id = req.params.id;
             var currentPage = req.query.page || 1;
-            follow.getFollowAllData(selfid, id, currentPage).then(function(data) {
-                //渐变圈
-                if (id == 'add') {
-                    res.render('follow-add', {
-                        title: '渐变-添加关注',
-                        user: req.session.user,
-                        data,
-                    });
-                } else {
-                    res.render('follow', {
-                        title: '渐变-关注',
-                        user: req.session.user,
-                        data,
-                    });
-                }
-            });
+            follow
+                .getFollowAllData(selfid, id, currentPage)
+                .then(function (data) {
+                    //渐变圈
+                    if (id == "add") {
+                        res.render("follow-add", {
+                            title: "渐变-添加关注",
+                            user: req.session.user,
+                            data
+                        });
+                    } else {
+                        res.render("follow", {
+                            title: "渐变-关注",
+                            user: req.session.user,
+                            data
+                        });
+                    }
+                });
         } else {
-            res.redirect('/login');
+            res.redirect("/login");
         }
     });
 
     //消息
-    app.get('/message', function(req, res) {
+    app.get("/message", function (req, res) {
         if (req.session.user) {
-            res.render('message', {
-                title: '渐变-消息',
+            res.render("message", {
+                title: "渐变-消息",
                 user: req.session.user
             });
         } else {
-            res.redirect('/login');
+            res.redirect("/login");
         }
     });
 
     //个人中心
-    app.get('/personal/:type/:id', function(req, res) {
+    app.get("/personal/:type/:id", function (req, res) {
         if (req.session.user) {
             var title;
             //个人中心类型
@@ -126,145 +133,159 @@ module.exports = function(app) {
                 isSelf = false;
             }
             var currentPage = req.query.page || 1;
-            if (type != 'setting') {
-                if (type == 'article') {
-                    title = '渐变-文章';
-                    personal.getPersonalArticle(id, sid, currentPage).then(function(data) {
-                        res.render('personal-article', {
-                            goto,
-                            title,
-                            user: req.session.user,
-                            data,
-                            isSelf,
+            if (type != "setting") {
+                if (type == "article") {
+                    title = "渐变-文章";
+                    personal
+                        .getPersonalArticle(id, sid, currentPage)
+                        .then(function (data) {
+                            res.render("personal-article", {
+                                goto,
+                                title,
+                                user: req.session.user,
+                                data,
+                                isSelf
+                            });
                         });
-                    });
-                } else if (type == 'follow' || type == 'fans') {
-                    title = type == 'follow' ? '渐变-关注' : '渐变-粉丝';
-                    personal.getPersonalFollowAndFans(id, sid, currentPage, type).then(function(data) {
-                        res.render('personal-' + type, {
-                            goto,
-                            title,
-                            user: req.session.user,
-                            data,
-                            isSelf,
+                } else if (type == "follow" || type == "fans") {
+                    title = type == "follow" ? "渐变-关注" : "渐变-粉丝";
+                    personal
+                        .getPersonalFollowAndFans(id, sid, currentPage, type)
+                        .then(function (data) {
+                            res.render("personal-" + type, {
+                                goto,
+                                title,
+                                user: req.session.user,
+                                data,
+                                isSelf
+                            });
                         });
-                    });
-                } else if (type == 'collectarticle' || type == 'likearticle') {
-                    title = type == 'collectarticle' ? '渐变-收藏的文章' : '渐变-喜欢的文章';
-                    personal.getPersonalLikeAndCollect(id, sid, currentPage, type).then(function(data) {
-                        res.render('personal-' + type, {
-                            goto,
-                            title,
-                            user: req.session.user,
-                            data,
-                            isSelf,
+                } else if (type == "collectarticle" || type == "likearticle") {
+                    title =
+                        type == "collectarticle" ?
+                            "渐变-收藏的文章" :
+                            "渐变-喜欢的文章";
+                    personal
+                        .getPersonalLikeAndCollect(id, sid, currentPage, type)
+                        .then(function (data) {
+                            res.render("personal-" + type, {
+                                goto,
+                                title,
+                                user: req.session.user,
+                                data,
+                                isSelf
+                            });
                         });
-                    });
                 }
             } else {
-                setting.getProfile(id).then(function(data) {
-                    res.render('personal-setting', {
-                        title: '渐变-设置',
+                setting.getProfile(id).then(function (data) {
+                    res.render("personal-setting", {
+                        title: "渐变-设置",
                         user: req.session.user,
-                        data,
+                        data
                     });
                 });
             }
         } else {
-            res.redirect('/login');
+            res.redirect("/login");
         }
     });
 
     //写文章
-    app.get('/write', function(req, res) {
+    app.get("/write", function (req, res) {
         if (req.session.user) {
-            write.getArticle(req.session.user.id).then(function(data){
-                res.render('write', {
-                    title: '写文章',
+            write.getArticle(req.session.user.id).then(function (data) {
+                res.render("write", {
+                    title: "写文章",
                     user: req.session.user,
                     data
                 });
             });
         } else {
-            res.redirect('/login');
+            res.redirect("/login");
         }
     });
 
-    app.get('/write/:articleid', function(req, res) {
+    app.get("/write/:articleid", function (req, res) {
         if (req.session.user) {
             var articleid = req.params.articleid;
-            write.getArticle(req.session.user.id, articleid).then(function(data){
-                res.render('write', {
-                    title: '写文章',
-                    user: req.session.user,
-                    data
+            write
+                .getArticle(req.session.user.id, articleid)
+                .then(function (data) {
+                    if (!data.singlearticle) {
+                        res.redirect("/you-don't-have-this-article");
+                    } else {
+                        res.render("write", {
+                            title: "写文章",
+                            user: req.session.user,
+                            data
+                        });
+                    }
                 });
-            });
         } else {
-            res.redirect('/login');
+            res.redirect("/login");
         }
     });
 
     //文章浏览
-    app.get('/article/:userid/:articleid', function(req, res) {
+    app.get("/article/:userid/:articleid", function (req, res) {
         var userid = req.params.userid;
         var articleid = req.params.articleid;
-        var sid = req.session.user ? req.session.user.id : 'no';
-        article.getArticle(userid, sid, articleid).then(function(data) {
-            res.render('article', {
-                title: '文章',
-                user: req.session.user || 'no',
-                data,
+        var sid = req.session.user ? req.session.user.id : "no";
+        article.getArticle(userid, sid, articleid).then(function (data) {
+            res.render("article", {
+                title: "文章",
+                user: req.session.user || "no",
+                data
             });
         });
     });
 
-    app.get('/about', function(req, res) {
-        res.render('about', {
-            title: '关于',
-            user: req.session.user || 'no'
-        })
+    app.get("/about", function (req, res) {
+        res.render("about", {
+            title: "关于",
+            user: req.session.user || "no"
+        });
     });
 
-    //保存文章
-    app.post('/savearticle', write.saveArticle);
+    //发布文章
+    app.post("/publisharticle", write.publishArticle);
 
     //添加文章分类
-    app.post('/addcategory', write.addCategory);
+    app.post("/addcategory", write.addCategory);
 
     //删除文章
-    app.post('/deletearticle', personal.deleteArticle);
+    app.post("/deletearticle", personal.deleteArticle);
 
     //存储头像
-    app.post('/saveHeadImage', setting.saveHeadImage);
+    app.post("/saveHeadImage", setting.saveHeadImage);
 
     //保存资料
-    app.post('/saveprofile', setting.saveProfile);
+    app.post("/saveprofile", setting.saveProfile);
 
     //修改密码
-    app.post('/changepassword', setting.changepassword);
+    app.post("/changepassword", setting.changepassword);
 
     //关注用户
-    app.post('/followperson', getIndexData.followPerson);
+    app.post("/followperson", getIndexData.followPerson);
 
     //取消关注用户
-    app.post('/unfollowperson', getIndexData.unFollowPerson);
+    app.post("/unfollowperson", getIndexData.unFollowPerson);
 
     //喜欢收藏文章
-    app.post('/doarticle', article.doArticle);
+    app.post("/doarticle", article.doArticle);
 
     //取消喜欢收藏文章
-    app.post('/undoarticle', article.undoArticle);
-
+    app.post("/undoarticle", article.undoArticle);
 
     //退出登录
-    app.post('/logout', function(req, res) {
+    app.post("/logout", function (req, res) {
         req.session.destroy();
         res.json({});
     });
 
     //404页面
-    app.get('*', function(req, res) {
-        res.render('404');
+    app.get("*", function (req, res) {
+        res.render("404");
     });
-}
+};
--- routes/write.js
+++ routes/write.js
@@ -1,74 +1,140 @@
-var Article = require('../schemas/article');
-var User = require('../schemas/user');
+var Article = require("../schemas/article");
+var User = require("../schemas/user");
 
-var saveArticle = function(req, res) {
+var publishArticle = function (req, res) {
     var data = req.body;
     var id = req.session.user.id;
     //用户文章数+1
-    User.findByIdAndUpdate(id, { '$inc': { 'articleNum': 1 } }, function(err, val) {
+    User.findByIdAndUpdate(id, {
+        $inc: {
+            articleNum: 1
+        }
+    }, function (err, val) {
         if (err) {
             console.log(err);
             return err;
         }
     });
 
-    //插入新的文章
-    var a = new Article({
-        authorId: id,
-        title: data.title,
-        tag: data.tag,
-        content: data.content,
-        text: data.text
-    });
-    a.save(function(err, a) {
-        User.findByIdAndUpdate(id, { '$push': { 'articleList': { 'articleId': a._id } } }, function(err, art) {
-            res.json({ result: 'ok' });
+    if (!data.art_id) {
+        //插入新的文章
+        var a = new Article({
+            authorId: id,
+            title: data.title,
+            tag: data.tag,
+            category: data.category,
+            content: data.content,
+            text: data.text
         });
-    });
-}
+        a.save(function (err, a) {
+            User.findByIdAndUpdate(id, {
+                $push: {
+                    articleList: {
+                        articleId: a._id
+                    }
+                },
+                $pull: {
+                    categoryList: {
+                        categoryName: data.category
+                    }
+                }
+            }, function (err, art) {
+                var newNum;
+                for (let i = 0; i < art.categoryList.length; i++) {
+                    var cl = art.categoryList[i];
+                    if (cl.categoryName == data.category) {
+                        newNum = cl.categoryNum + 1;
+                        break;
+                    }
+                }
+                User.findByIdAndUpdate(id, {
+                    $push: {
+                        categoryList: {
+                            categoryName: data.category,
+                            categoryNum: newNum
+                        }
+                    }
+                }, function (err, art2) {
+                    res.json({
+                        result: "ok"
+                    });
+                });
+            });
+        });
+    } else {
+        Article.findByIdAndUpdate(data.art_id, {
+            title: data.title,
+            tag: data.tag,
+            category: data.category,
+            content: data.content,
+            text: data.text
+        }, function (err, art) {
+            res.json({
+                result: "publish",
+                aid: data.art_id
+            });
+        });
+    }
+};
 
-var getArticle = function (userid, articleid){
-    return new Promise(function(resolve,reject){
+var getArticle = function (userid, articleid) {
+    return new Promise(function (resolve, reject) {
         var data = [];
-        if (articleid){
+        if (articleid) {
             User.findById(userid, function (err, user) {
                 data.user = user;
-                Article.find({ "authorId": userid }).sort({ "createTime": -1 }).exec(function (err, arti) {
+                Article.find({
+                    authorId: userid
+                }).sort({
+                    createTime: -1
+                }).exec(function (err, arti) {
                     data.article = arti;
-                    Article.findById(articleid,function(err,singlearticle){
-                        data.singlearticle = singlearticle;
+                    Article.find({
+                        _id: articleid,
+                        authorId: userid
+                    }, function (err, singlearticle) {
+                        if (singlearticle.length != 0) {
+                            data.singlearticle = singlearticle;
+                        }
                         resolve(data);
                     });
                 });
             });
-        }else{
+        } else {
             User.findById(userid, function (err, user) {
                 data.user = user;
-                Article.find({ "authorId": userid }).sort({"createTime":-1}).exec(function (err, arti) {
+                Article.find({
+                    authorId: userid
+                }).sort({
+                    createTime: -1
+                }).exec(function (err, arti) {
                     data.article = arti;
-                    data.singlearticle = "";
+                    data.singlearticle = [{ category: "", content: "" }];
                     resolve(data);
                 });
             });
         }
     });
-}
+};
 
-var addCategory = function(req,res){
+var addCategory = function (req, res) {
     var newCategoryName = req.body.newCategoryName;
     var userid = req.session.user.id;
-    User.findByIdAndUpdate(userid, { '$push': { 'categoryList': { 'categoryName': newCategoryName}}},function(err,user){
-        console.log(user)
-        if(err){
-            console.log(err);
-            return err;
+    User.findByIdAndUpdate(userid, {
+        $push: {
+            categoryList: {
+                categoryName: newCategoryName
+            }
         }
-        res.json({result:'ok'});
+    }, function (err, user) {
+        res.json({
+            result: "ok"
+        });
     });
-}
+};
 
 module.exports = {
-    saveArticle,
+    publishArticle,
     getArticle,
-    addCategory,
-}
+    addCategory
+};
